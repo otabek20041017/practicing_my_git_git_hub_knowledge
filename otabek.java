c5708a99d72ee86d8a26ee3b219d7fdc173786e9
otabek ozbeg <otabek.yusupov20041017@gmail.com>
2025-01-05 09:57:57 +0500

    This is my first time commiting my otabek.java class on VS code

---
+++ otabek.java
@@ -0,0 +1,14 @@
+public class otabek {
+    
+    public static void main(String[] args) {
+        
+        int age = 20;
+        String name = "Otabek ";
+        String middleName = "Ozbeg";
+
+        System.out.println("Otabek's age = " + age + "\n" + name + middleName);
+        
+        
+    }
+
+}
